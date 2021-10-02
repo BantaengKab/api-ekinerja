2c0e4b468d1f2b9699a10fe21e2ede1d9cfbfe26
Asdar Muhammad <muhammadasdar5@gmail.com>
2021-10-02 22:53:07 +0800

    dataAbsen

--- app/AbsenLog.php
+++ app/AbsenLog.php
@@ -14,6 +14,12 @@ class AbsenLog extends Model
         'tanggal', 'masuk', 'istirahat', 'masuk2', 'pulang', 'kd_skpd', 'nip', 'status'
     ];
 
+
+
+    public function dataAbsen()
+    {
+        return $this->hasMany(AbsenData::class, 'absen_id', 'id');
+    }
     # status
     # 0. biasa
     # 1. jumat
--- app/Http/Controllers/API/AbsenController.php
+++ app/Http/Controllers/API/AbsenController.php
@@ -21,7 +21,7 @@ class AbsenController extends Controller
      */
     public function index()
     {
-        $data = AbsenLog::get();
+        $data = AbsenLog::with('dataAbsen')->get();
         return ResponseFormatter::success([
             "list" => $data,
         ], 'Authenticated', 200);
@@ -79,9 +79,7 @@ class AbsenController extends Controller
                     $dt->kd_absen = $jam->kd_absen;
                     $dt->save();
 
-                    return ResponseFormatter::success([
-                        "message" => "Berhasil absen",
-                    ], 'Authenticated', 200);
+                    return ResponseFormatter::success(["message" => "Berhasil absen"], 'Authenticated', 200);
                 } elseif ($jam->kd_absen == 1 && $absen->first()['istirahat'] == "") {
                     $log->first()->update(['istirahat' => $jam_skrang]);
 
@@ -90,9 +88,7 @@ class AbsenController extends Controller
                     $dt->kd_absen = $jam->kd_absen;
                     $dt->save();
 
-                    return ResponseFormatter::success([
-                        "message" => "Berhasil absen",
-                    ], 'Authenticated', 200);
+                    return ResponseFormatter::success(["message" => "Berhasil absen"], 'Authenticated', 200);
                 } elseif ($jam->kd_absen == 2 && $absen->first()['masuk2'] == "") {
                     $log->first()->update(['masuk2' => $jam_skrang]);
 
@@ -101,9 +97,7 @@ class AbsenController extends Controller
                     $dt->kd_absen = $jam->kd_absen;
                     $dt->save();
 
-                    return ResponseFormatter::success([
-                        "message" => "Berhasil absen",
-                    ], 'Authenticated', 200);
+                    return ResponseFormatter::success(["message" => "Berhasil absen"], 'Authenticated', 200);
                 } elseif ($jam->kd_absen == 3 && $absen->first()['pulang'] == "") {
                     $log->first()->update(['pulang' => $jam_skrang]);
 
@@ -113,21 +107,16 @@ class AbsenController extends Controller
                     $dt->kd_absen = $jam->kd_absen;
                     $dt->save();
 
-                    return ResponseFormatter::success([
-                        "message" => "Berhasil absen",
-                    ], 'Authenticated', 200);
+                    return ResponseFormatter::success(["message" => "Berhasil absen"], 'Authenticated', 200);
                 } elseif ($jam->kd_absen == 4 && $absen->first()['masuk'] == "") {
                     $log->first()->update(['masuk' => $jam_skrang]);
 
                     $gambar->move('dokumen/', $filename_gambar);
 
-
                     $dt->kd_absen = $jam->kd_absen;
                     $dt->save();
 
-                    return ResponseFormatter::success([
-                        "message" => "Berhasil absen",
-                    ], 'Authenticated', 200);
+                    return ResponseFormatter::success(["message" => "Berhasil absen"], 'Authenticated', 200);
                 } elseif ($jam->kd_absen == 5 && $absen->first()['pulang'] == "") {
                     $log->first()->update(['pulang' => $jam_skrang]);
 
@@ -135,13 +124,9 @@ class AbsenController extends Controller
                     $dt->kd_absen = $jam->kd_absen;
                     $dt->save();
 
-                    return ResponseFormatter::success([
-                        "message" => "Berhasil absen",
-                    ], 'Authenticated', 200);
+                    return ResponseFormatter::success(["message" => "Berhasil absen"], 'Authenticated', 200);
                 } else {
-                    return ResponseFormatter::error([
-                        "message" => "Anda sudah absen",
-                    ], 'Authenticated');
+                    return ResponseFormatter::error(["message" => "Anda sudah absen"], 'Authenticated');
                 }
 
                 // return $log->first()['id'];
