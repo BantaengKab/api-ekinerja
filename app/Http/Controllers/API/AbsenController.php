1c88c7333897f8b6340ff0642f585c613680db96
Asdar Muhammad <muhammadasdar5@gmail.com>
2021-10-02 21:20:25 +0800

    jam

--- app/Http/Controllers/API/AbsenController.php
+++ app/Http/Controllers/API/AbsenController.php
@@ -56,9 +56,9 @@ class AbsenController extends Controller
         $jam_skrang =  date('H:i:s');
         // return $jam_skrang;
         foreach ($jamKerja as $jam) {
+            return 1;
             if ($jam->jam_awal >= $jam_skrang && $jam->jam_akhir <= $jam_skrang) {
                 $absen = AbsenLog::whereDate('tanggal', '=', date('Y-m-d'))->where('nip', Auth::user()->username);
-                return 1;
                 if ($absen->count() == 0) AbsenLog::create(['tanggal' => date('Y-m-d'), 'kd_skpd' => Auth::user()->kd_skpd, 'username' => Auth::user()->username]);
 
                 $log = '';
