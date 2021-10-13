58ef50c377ba37855b83ff5851185366262e08d0
asdarmld <muhammadasdar5@gmail.com>
2021-10-13 23:38:43 +0700

    app

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -14,85 +14,85 @@ class UserController extends Controller
 {
     public function login(Request $request)
     {
-        // try {
-        #validasi input
-        // $validator =  $request->validate([
-        //     'email' => 'required|email',
-        //     'password' => 'required'
-        // ]);
+        try {
+            #validasi input
+            // $validator =  $request->validate([
+            //     'email' => 'required|email',
+            //     'password' => 'required'
+            // ]);
 
-        $validator = Validator::make($request->all(), [
-            'username' => 'required',
-            'pass' => 'required',
-            'kd_perangkat' => 'required',
-        ]);
+            $validator = Validator::make($request->all(), [
+                'username' => 'required',
+                'pass' => 'required',
+                'kd_perangkat' => 'required',
+            ]);
 
-        if ($validator->fails()) {
-            return ResponseFormatter::error([
-                "error" => 'validation_error',
-                "message" => $validator->errors(),
-            ], 'Authentication Failed', 422);
-        }
-
-
-        # mengecek credentials (login)
-        $credentials = request(['username', 'pass']);
-
-
-
-        // if (!Auth::attempt($credentials)) {
-        //     return ResponseFormatter::error([
-        //         'message' => 'Unauthorized'
-        //     ], 'Authentication Failed', 500);
-        // }
-
-        # Jika hash tidak sesuai
-        $user = User::where('username', $request->username);
-        $kd_perangkat = randomString(10);
-
-        $md5 = md5($user->first()['id']);
-        $hash = hash('sha256', $md5 . $request->pass);
-
-        if ($user->first()['pass'] != $hash) {
-            // return 22;
-            throw new \Exception('Invalid Credentials');
-        }
-
-        // if (!Hash::check($request->password, $user->password, [])) {
-        //     throw new \Exception('Invalid Credentials');
-        // }
-
-        # cek perangkat yang digunakan
-        if ($user->first()['kd_perangkat'] ==  null || $user->first()['kd_perangkat'] ==  '') {
-            User::where('id', $user->first()['id'])
-                ->update([
-                    'kd_perangkat' => $kd_perangkat,
-                ]);
-        } else {
-            if ($user->first()['kd_perangkat'] !=  $request->kd_perangkat) {
+            if ($validator->fails()) {
                 return ResponseFormatter::error([
-                    'message' => 'Silahkan gunakan perangkat pribadi anda',
-                ], 'Device not found', 501);
+                    "error" => 'validation_error',
+                    "message" => $validator->errors(),
+                ], 'Authentication Failed', 422);
             }
+
+
+            # mengecek credentials (login)
+            $credentials = request(['username', 'pass']);
+
+
+
+            // if (!Auth::attempt($credentials)) {
+            //     return ResponseFormatter::error([
+            //         'message' => 'Unauthorized'
+            //     ], 'Authentication Failed', 500);
+            // }
+
+            # Jika hash tidak sesuai
+            $user = User::where('username', $request->username);
+            $kd_perangkat = randomString(10);
+
+            $md5 = md5($user->first()['id']);
+            $hash = hash('sha256', $md5 . $request->pass);
+
+            if ($user->first()['pass'] != $hash) {
+                // return 22;
+                throw new \Exception('Invalid Credentials');
+            }
+
+            // if (!Hash::check($request->password, $user->password, [])) {
+            //     throw new \Exception('Invalid Credentials');
+            // }
+
+            # cek perangkat yang digunakan
+            if ($user->first()['kd_perangkat'] ==  null || $user->first()['kd_perangkat'] ==  '') {
+                User::where('id', $user->first()['id'])
+                    ->update([
+                        'kd_perangkat' => $kd_perangkat,
+                    ]);
+            } else {
+                if ($user->first()['kd_perangkat'] !=  $request->kd_perangkat) {
+                    return ResponseFormatter::error([
+                        'message' => 'Silahkan gunakan perangkat pribadi anda',
+                    ], 'Device not found', 501);
+                }
+            }
+
+            # jika berhasil maka loginkan
+            $tokenResult = $user->first()->createToken($user->first()['full_name']);
+
+            $token = $tokenResult->token;
+            $token->save();
+            return ResponseFormatter::success([
+                'access_token' => $tokenResult->accessToken,
+                'token_type' => 'Bearer',
+                'user' => $user->first()
+            ], 'Authenticated');
+        } catch (Exception $error) {
+            return ResponseFormatter::error([
+                'message' => 'Something went error',
+                'error' => $error,
+
+            ], 'Authentication Failed', 500);
         }
-
-        # jika berhasil maka loginkan
-        $tokenResult = $user->first()->createToken($user->first()['full_name']);
-
-        $token = $tokenResult->token;
-        $token->save();
-        return ResponseFormatter::success([
-            'access_token' => $tokenResult->accessToken,
-            'token_type' => 'Bearer',
-            'user' => $user->first()
-        ], 'Authenticated');
-        // } catch (Exception $error) {
-        //     return ResponseFormatter::error([
-        //         'message' => 'Something went error',
-        //         'error' => $error,
-
-        //     ], 'Authentication Failed', 500);
-        // }
     }
 
     public function fetch(Request $request)
