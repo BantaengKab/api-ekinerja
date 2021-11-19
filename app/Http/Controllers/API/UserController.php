5e7ee6d2f21779cceb1470e10b0410b4752e4dc3
asdarmld <muhammadasdar5@gmail.com>
2021-11-19 08:01:32 +0700

    auth

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -74,7 +74,8 @@ class UserController extends Controller
                         'kd_perangkat' => $kd_perangkat,
                     ]);
             } else {
-                if ($user->first()['username'] != 'asdarmld' || $user->first()['username'] != '198702142006041002') {
+
+                if ($request->username != 'asdarmld' || $request->username != '198702142006041002') {
                     if ($user->first()['kd_perangkat'] !=  $request->kd_perangkat) {
                         return ResponseFormatter::error([
                             'message' => 'Silahkan gunakan perangkat pribadi anda',
